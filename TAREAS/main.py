57cab91109a975c93ed655b7f8484ae1c647ac34
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-02-18 17:20:23 -0600

    Endpoint2_id_terminado

--- TAREAS/main.py
+++ TAREAS/main.py
@@ -10,7 +10,7 @@ app= FastAPI(
 #diccionario o lsita de objetos
 tareas=[
     {"id":1, "Titulo":"Repasar los apuntes de virtualización", "Descripcion":"Aprender tipos de virtualización", "Vencimiento":"11-02-25", "Estado":"Completada"},
-    {"id":2, "Titulo":"Hacer un proyecto nuevo de FastAPI ", "Descripcion":"Repasar todos los temas vistos hast el momento", "Vencimiento":"20-02-25", "Estado":"Pendiente"},
+    {"id":2, "Titulo":"Hacer un proyecto nuevo de FastAPI ", "Descripcion":"Repasar todos los temas vistos hasta el momento", "Vencimiento":"20-02-25", "Estado":"Pendiente"},
     {"id":3, "Titulo":"Vocabulario de Ingles", "Descripcion":"Repasar lista de verbos irregulares", "Vencimiento":"23-02-25", "Estado":"Pendiente"},
     {"id":4, "Titulo":"Diagramas UML", "Descripcion":"Hacer los diagramas de secuencia y casos de uso del proyecto", "Vencimiento":"12-02-25", "Estado":"Completada"},
     {"id":5, "Titulo":"Presentacion de interaccion de maquina-computadora", "Descripcion":"Investigar sobre los perifericos y como es su interaccion con la maquina", "Vencimiento":"13-02-25", "Estado":"Completada"},
@@ -18,6 +18,16 @@ tareas=[
 
 
 #Endpoint CONSULTAR Tareas
-@app.get('/todasTareas', tags=['Repaso Tareas'])
+@app.get('/todasTareas', tags=['Mostrar Tareas'])
 def verTareas():
     return{'Las tareas registradas son':tareas}
+
+#Endpoint para obtener una tarea por ID
+@app.get('/tareas/{id}', tags=['Mostrar Tarea por ID'])
+def tareaEsp(id: int):
+    for tarea in tareas:
+        if tarea["id"] == id: 
+            return tarea
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
+
+
